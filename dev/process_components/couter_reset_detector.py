a6e8f24891ecda51642c2ec720beed78eea6c6d2
andrazpolak <andrazpolak@gmail.com>
2025-03-31 19:19:23 +0200

    fds

--- dev/process_components/couter_reset_detector.py
+++ dev/process_components/couter_reset_detector.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from atptools.process_components import pandas_counter_reset_detector
 
-dfts = pd.DataFrame(
+df = pd.DataFrame(
     data={
         "name": [
             "value_1",
@@ -17,7 +17,15 @@ dfts = pd.DataFrame(
     },
     index=pd.date_range("2021-01-01", "2021-01-05", periods=7),
 )
+print("df:")
+print(df)
 
-list_1, list_2 = pandas_counter_reset_detector(dfts["value"])
-print(list_1)
-print(list_2)
+list_1, list_2 = pandas_counter_reset_detector(df["value"])
+print("Index counter zero (low):", list_1)
+print("Index counter high", list_2)
+
+# or
+
+list_1, list_2 = pandas_counter_reset_detector(df, value_col="value")
+print("Index counter zero (low):", list_1)
+print("Index counter high", list_2)
